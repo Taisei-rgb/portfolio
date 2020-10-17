ab65e25e5445e90a17fa8bec43f88eee5fb277fe
Taisei-rgb <taisei.contact@gmail.com>
2020-10-17 18:18:08 +0900

    home画面の実装

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import "./home.css"
 
 class Home extends Component {
   render() {
     return (
-      <div><h1>HOME</h1></div>
+      <div className="wrapper">
+          <div className="home__name">
+            <div className="name" >Hi. I'm <span className="marker">Taisei Katagiri</span></div>
+            <div className="home__discription">
+              Thanks for viewing my portfolio.<br></br>
+              I am a Japanese Web Developper, Artist based in Tokyo. Born in Fukushima.
+            </div>
+          </div>
+      </div>
     )
   }
 }
