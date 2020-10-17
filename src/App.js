e219ce151c3a23f382b0c61243de85328cf5bc2a
Taisei-rgb <taisei.contact@gmail.com>
2020-10-17 14:59:45 +0900

    header/各componentの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Content } from "react-mdl"
 
-function App() {
-  return (
-    <h1>Hello React!</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{ height: '300px', position: 'relative' }}>
+        <Layout fixedHeader>
+          <Header
+            className="header-color"
+            title={<span style={{ color: 'black' }} >TAISEI KATAGIRI</span>}>
+            <Navigation >
+              <a href="#" style={{ color: 'black' }}>HOME</a>
+              <a href="#" style={{ color: 'black' }}>ABOUT</a>
+              <a href="#" style={{ color: 'black' }}>WORK</a>
+              <a href="#" style={{ color: 'black' }}>CONTACT</a>
+            </Navigation>
+          </Header>
+          <Content>
+            <div className="page-content" />
+          </Content>
+          <Content />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
